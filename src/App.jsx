3a89e11a9c93e8d21be68bfa14cc52509c52525d
Tal Dwek <tal.dwek@zoominsoftware.com>
2022-08-11 15:57:22 +0300

    fix bug

--- src/App.jsx
+++ src/App.jsx
@@ -71,7 +71,7 @@ const App = () => {
   useEffect(() => {
     if (weather) {
     const interval = setInterval(async () => {
-      getAndSetWeather(weather.location)
+      getAndSetWeather(weather.currentWeather.city)
     } ,5000);
     return () => clearTimeout(interval);
     }
